fe585ca7239f9fb525bca80bc58230dbb78e853e
Mahmoud Ahmed <mahmoudahmed18840@gmail.com>
2025-05-20 15:19:41 +0300

    finish product  filteration

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
 import { ShopService } from './shop.service';
 import { IPagination } from '../core/shared/Models/Pagination';
 import { IProduct } from '../core/shared/Models/Products';
+import { ICategory } from '../core/shared/Models/Category';
 
 @Component({
   selector: 'app-shop',
@@ -10,23 +17,74 @@ import { IProduct } from '../core/shared/Models/Products';
   styleUrl: './shop.component.scss',
 })
 export class ShopComponent implements OnInit {
-  public product: IProduct[] 
-  /**
-   *
-   */
+  public product: IProduct[];
+  public category: ICategory[];
+  public categoryId: number;
+  public sortSelected: string = 'PriceDes';
+
   constructor(private shopService: ShopService) {}
+
   ngOnInit(): void {
+    console.log('Initial sortSelected:', this.sortSelected);
     this.GetAllProduct();
+    this.GetAllCategory();
   }
+
   GetAllProduct() {
-    this.shopService.getProduct().subscribe({
-      next: (value: IPagination) => {
-        // console.log(value.data);
-        this.product = value.data;
-      },
-      error: (err: any) => {
-        console.log(err);
+    console.log('Getting products with sort:', this.sortSelected, this.search);
+    this.shopService
+      .getProduct(this.categoryId, this.sortSelected, this.search)
+      .subscribe({
+        next: (value: IPagination) => {
+          console.log('Products received:', value.data);
+          this.product = value.data;
+        },
+        error: (err: any) => {
+          console.error('Error fetching products:', err);
+          this.product = [];
+        },
+      });
+  }
+
+  GetAllCategory() {
+    this.shopService.getCategory().subscribe({
+      next: (value: any) => {
+        this.category = value;
       },
     });
+  }
+
+  SelectedId(id: number) {
+    this.categoryId = id;
+    this.GetAllProduct();
+  }
+
+  SortingOption = [
+    { name: 'Price', value: 'Name' },
+    { name: 'Price:max-min', value: 'PriceDes' },
+    { name: 'Price:min-max', value: 'PriceAsn' },
+  ];
+
+  SortingByPrice(event: any) {
+    const newSort = event.target.value;
+    console.log('Sort changed from', this.sortSelected, 'to', newSort);
+    this.sortSelected = newSort;
+    this.GetAllProduct();
+  }
+  //filter product by word
+  search: string;
+  OnSearch(search: string) {
+    this.search = search;
+    this.GetAllProduct();
+  }
+  @ViewChild('search') searchInput: ElementRef;
+  @ViewChild('selectItem') selectItem: ElementRef;
+  OnReset() {
+    this.searchInput.nativeElement.value = '';
+    this.search = '';
+    this.categoryId = 0;
+    this.selectItem.nativeElement.selectedIndex = 0;
+    this.sortSelected = this.SortingOption[0].value;
+    this.GetAllProduct();
   }
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPagination } from '../core/shared/Models/Pagination';
 import { IProduct } from '../core/shared/Models/Products';
+import { ICategory } from '../core/shared/Models/Category';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +12,27 @@ export class ShopService {
   constructor(private http: HttpClient) {}
   baseUrl = 'https://localhost:44306/api/';
 
-  getProduct() {
+  getProduct(categoryId?: number, sortSelected?: string, search?: string) {
+    let param = new HttpParams();
+    if (categoryId && categoryId != 0) {
+      param = param.append('CategotyId', categoryId);
+    }
+    if (sortSelected) {
+      param = param.append('sort', sortSelected);
+    }
+    if (search) {
+      param = param.append('Search', search);
+    }
+    console.log('API Parameters:', param.toString());
+    console.log('Full URL:', this.baseUrl + 'Product/get-all?' + param.toString());
+
     return this.http.get<IPagination>(
-      this.baseUrl + 'Product/get-all?sort=a'
+      this.baseUrl + 'Product/get-all',
+      { params: param }
     );
+  }
+  getCategory()
+  {
+    return this.http.get<ICategory[]>(this.baseUrl + 'Categoty/get-all');
   }
 }
